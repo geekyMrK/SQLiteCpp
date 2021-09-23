4c106830bd805da36f45f7f637411ab624a30f65
geekyMrK <85291864+geekyMrK@users.noreply.github.com>
2021-09-23 21:26:01 +0530

    Create SqliteWrapper.hpp
    Added SqliteRow and rowIterator to iterate over the statement using the for-each loop

---
+++ include/SqliteWrapper.hpp
@@ -0,0 +1,50 @@
+#include "SqliteStatement.hpp"
+
+namespace Sqlite {
+
+class SqliteRow : public sqliteReader<SqliteRow> {
+	sqlite3_stmt *statement_{nullptr};
+
+  public:
+  
+	constexpr sqlite3_stmt *getABI() const noexcept {
+		return statement_;
+	}
+	
+	SqliteRow(sqlite3_stmt *const statement) noexcept : statement_{statement} {
+	}
+};
+
+
+class rowIterator {
+	
+	const SqliteStatement const *statement_{nullptr};
+
+  public:
+	rowIterator() noexcept = default;
+	rowIterator(const SqliteStatement &statement) noexcept {
+		if (statement.execute()) {
+			statement_ = &statement;
+		}
+	}
+	
+	rowIterator &operator++() noexcept ;
+
+	constexpr bool operator!=(const rowIterator &other) const noexcept {
+		return statement_ != other.statement_;
+	}
+
+	SqliteRow operator*() const noexcept ;
+};
+
+
+inline rowIterator begin(const SqliteStatement &statement) noexcept {
+	return rowIterator(statement);
+}
+
+constexpr inline rowIterator end(const SqliteStatement &) noexcept {
+	return rowIterator();
+}
+
+
+}
