10aada27bf7e5419a0a56a799725fc3db1355260
Mr.K <85291864+geekyMrK@users.noreply.github.com>
2021-09-30 14:03:59 +0530

    Update SqliteStatement.hpp
    Added constexpr

--- include/SqliteStatement.hpp
+++ include/SqliteStatement.hpp
@@ -4,21 +4,21 @@ namespace Sqlite{
   template <typename T>
   struct sqliteReader {
 	  
-       int getInt(const int columnNum = 0) const noexcept {
+        constexpr int getInt(const int columnNum = 0) const noexcept {
 		return sqlite3_column_int(static_cast<const T *>(this)->getABI(), columnNum);
 	}
 
-	const char *getString(const int columnNum = 0) const noexcept {
+	constexpr const char *getString(const int columnNum = 0) const noexcept {
 		return reinterpret_cast<const char *>(sqlite3_column_text(static_cast<const T *>(this)->getABI(), columnNum));
 	}
 
-	const wchar_t *getWideString(const int columnNum = 0) const noexcept {
+	constexpr const  wchar_t *getWideString(const int columnNum = 0) const noexcept {
 		return static_cast<const wchar_t *>(sqlite3_column_text16(static_cast<const T *>(this)->getABI(), columnNum));
 	}
 
-	int getStringLength(const int columnNum) const ;
+	constexpr int getStringLength(const int columnNum) const ;
 	
-	int getWideStringLength(const int columnNum) const noexcept ;
+	constexpr int getWideStringLength(const int columnNum) const noexcept ;
 
   };
 	
