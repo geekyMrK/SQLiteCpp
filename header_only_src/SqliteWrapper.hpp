5c9740f758ff40bdf269c77a70d7143557554919
Mr.K <85291864+geekyMrK@users.noreply.github.com>
2021-09-30 14:08:27 +0530

    Update SqliteWrapper.hpp
    Added constexpr

--- header_only_src/SqliteWrapper.hpp
+++ header_only_src/SqliteWrapper.hpp
@@ -174,23 +174,23 @@ class SqliteConnection {
 template <typename T>
 struct sqliteReader {
 	
-	int getInt(const int columnNum = 0) const noexcept {
+	constexpr int getInt(const int columnNum = 0) const noexcept {
 		return sqlite3_column_int(static_cast<const T *>(this)->getABI(), columnNum);
 	}
 
-	const char *getString(const int columnNum = 0) const noexcept {
+	constexpr const char *getString(const int columnNum = 0) const noexcept {
 		return reinterpret_cast<const char *>(sqlite3_column_text(static_cast<const T *>(this)->getABI(), columnNum));
 	}
 
-	const wchar_t *getWideString(const int columnNum = 0) const noexcept {
+	constexpr const wchar_t *getWideString(const int columnNum = 0) const noexcept {
 		return static_cast<const wchar_t *>(sqlite3_column_text16(static_cast<const T *>(this)->getABI(), columnNum));
 	}
 
-	int getStringLength(const int columnNum) const noexcept {
+	constexpr int getStringLength(const int columnNum) const noexcept {
 		return sqlite3_column_bytes(static_cast<const T *>(this)->getABI(), columnNum);
 	}
 
-	 int getWideStringLength(const int columnNum) const noexcept {
+	constexpr int getWideStringLength(const int columnNum) const noexcept {
 		return (sqlite3_column_bytes16(static_cast<const T *>(this)->getABI(), columnNum) / sizeof(wchar_t));
 	}
 
